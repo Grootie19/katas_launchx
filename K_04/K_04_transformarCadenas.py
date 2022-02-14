9c8b9ea79c50518960cf03b8289537b8437812fc
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 11:44:27 -0600

    imprime cadenas segun busqueda

--- K_04/K_04_transformarCadenas.py
+++ K_04/K_04_transformarCadenas.py
@@ -9,18 +9,20 @@
 # On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
 
 # Texto dato
-from time import clock_settime
-
-
 text = """Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth.
 On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
 
 # Divide el texto en cada oración
 divText = text.split('.')
-print (divText)
+#print (divText)
 
 # Define palabras clave para busqueda
 firstkw = 'average'
 secondkw = 'temperature'
 thirdkw = 'distance'
 
+# Imprime los datos que contengan alguna de las pralabras claves
+for fact in divText:
+    fact.lower
+    if fact.count(firstkw) > 0 or fact.count(secondkw) or fact.count(thirdkw):
+        print(fact)
