24c87c5c3fd34ec387692b6c8f668f89491ad99d
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 13:04:01 -0600

    Inicializa el archivo con los datos sobre la kata

---
+++ K_04/K_04_formatearCadenas.py
@@ -0,0 +1,12 @@
+# Acomodar información sobre distintas lunas
+# La información debe presentarse en un formato tabular eg.
+
+# Gravity Facts about Ganymede
+# -------------------------------------------------------------------------------
+# Planet Name: Mars
+# Gravity on Ganymede: 1.4300000000000002 m/s2
+
+# Datos a utilizar
+# Moon, gravedad 0.00162 km/s, planet Earth
+
+
