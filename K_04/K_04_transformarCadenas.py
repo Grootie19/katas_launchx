35fb5b985a0c6d2589b0dc1f9a5860f84ea18e88
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 11:23:48 -0600

    define palabras clave para busqueda

--- K_04/K_04_transformarCadenas.py
+++ K_04/K_04_transformarCadenas.py
@@ -15,5 +15,12 @@ from time import clock_settime
 text = """Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth.
 On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
 
+# Divide el texto en cada oración
 divText = text.split('.')
 print (divText)
+
+# Define palabras clave para busqueda
+firstkw = 'average'
+secondkw = 'temperature'
+thirdkw = 'distance'
+
