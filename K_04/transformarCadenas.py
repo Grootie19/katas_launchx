fc39e30b252c07f2df73c25892a5a723fe8068eb
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 10:20:52 -0600

    crea variable para almacenar text.split

--- K_04/transformarCadenas.py
+++ K_04/transformarCadenas.py
@@ -9,6 +9,11 @@
 # On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
 
 # Texto dato
+from time import clock_settime
+
+
 text = """Interesting facts about the Moon. The Moon is Earth's only satellite. There are several interesting facts about the Moon and how it affects life here on Earth.
 On average, the Moon moves 4cm away from the Earth every year. This yearly drift is not significant enough to cause immediate effects on Earth. The highest daylight temperature of the Moon is 127 C."""
 
+divText = text.split('.')
+print (divText)
