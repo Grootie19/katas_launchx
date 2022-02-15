b215f896e11f12da7d769cacbdc90f08405fc870
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 19:32:41 -0600

    elimina la conversion de variables cadena

--- K_05/K_05_inputDistPlanetas.py
+++ K_05/K_05_inputDistPlanetas.py
@@ -10,8 +10,7 @@ inDistPlanet_02 = input("Distancia al sol (segundo planeta)")
 # Input devulve valores de tipo cadena
 
 #Convertir cadenas a números
-inNamePlanet_01 = int(inNamePlanet_01)
-inNamePlanet_02 = int(inNamePlanet_02)
 inDistPlanet_01 = int(inDistPlanet_01)
 inDistPlanet_02 = int(inDistPlanet_02)
 
+print(inDistPlanet_01)
