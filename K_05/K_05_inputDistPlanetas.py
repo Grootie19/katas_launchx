21627a427549c9ac1df4a506e3facf9c5fb3e122
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 19:42:59 -0600

    resuelve ejercicio 2 kata 5

--- K_05/K_05_inputDistPlanetas.py
+++ K_05/K_05_inputDistPlanetas.py
@@ -3,13 +3,19 @@
 
 # Variables
 inNamePlanet_01 = input("Nombre del primer Planeta: ")
-inDistPlanet_01 = input("Distancia al sol: ")
+inDistPlanet_01 = int(input("Distancia al sol: "))
 inNamePlanet_02 = input("Nombre del segundo Planeta: ")
-inDistPlanet_02 = input("Distancia al sol: ")
+inDistPlanet_02 = int(input("Distancia al sol: "))
 
 # Input devulve valores de tipo cadena
 
 #Convertir cadenas a números
-inDistPlanet_01 = int(inDistPlanet_01)
-inDistPlanet_02 = int(inDistPlanet_02)
+# inDistPlanet_01 = int(inDistPlanet_01)
+# inDistPlanet_02 = int(inDistPlanet_02)
+# Elima este bloque ta que se puede hacer la conversión desde la solicitud de los datos
+
+suma = inDistPlanet_01 - inDistPlanet_02
+
+print(f"La distancia entre {inNamePlanet_01} y {inNamePlanet_02} es: {abs(suma)}")
+
 
