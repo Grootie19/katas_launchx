005d804db8b3023aa9a9607c065a0a58f81e398e
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 19:26:51 -0600

    declara las variables como inputs

---
+++ K_05/K_05_inputDistPlanetas.py
@@ -0,0 +1,10 @@
+# Convertir los valores de cadena en números
+# utilizar inputs
+
+# Variables
+inNamePlanet_01 = input("Nombre del primer Planeta")
+inNamePlanet_02 = input("Nombre del segundo Planeta")
+inDistPlanet_01 = input("Distancia al sol (primer planeta)")
+inDistPlanet_02 = input("Distancia al sol (segundo planeta)")
+
+
