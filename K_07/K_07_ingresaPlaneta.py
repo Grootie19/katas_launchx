bbb4fe7183eadf8e787a8dea8eb63bfd74ed4a7b
isbaruc <launchx05286@innovaccion.mx>
2022-02-15 10:19:58 -0600

    Crea el archivo ejercicio 1 kata 7

---
+++ K_07/K_07_ingresaPlaneta.py
@@ -0,0 +1,14 @@
+# Crear un programa que permita al usuario añadir planetas a una lista
+
+# Declaracion de variables
+from hashlib import new
+
+new_planet = ''
+planets = []
+
+# Para terminar la lista el usuario ingresa la palabra 'done'
+while new_planet.lower() != 'done':
+    planets.append(new_planet)
+    new_planet = input("Nombre del planeta a agregar: ")
+
+print (planets)
