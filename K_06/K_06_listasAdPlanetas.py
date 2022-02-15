271e0065f87131b84a7e5413079eb6a1c0f80590
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 23:16:23 -0600

    agrega ejercicio 2 kata 6 realizado

--- K_06/K_06_listasAdPlanetas.py
+++ K_06/K_06_listasAdPlanetas.py
@@ -1,10 +1,29 @@
 # Crea la lista de planetas
+from operator import index
+from turtle import position
+
+
 planetas = ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Neptuno']
 
 namePlanet = input("Planeta que buscamos: ") 
 
 planetasL = []
 
+# Nombres de planetas a minusculas
 for planetaM in planetas[0:]:
     planetasL.append(planetaM.lower())
-    print(planetasL)
+
+# Entrada del usuario a minusculas
+namePlanetL = namePlanet.lower()
+
+try:
+# Busqueda con todas en minuscula
+    posPlanet = (planetasL.index(namePlanetL))
+    # print(planetasL.index(namePlanetL)) # Imprime la posicion del resultado de busqueda
+except: 
+    posPlanet = -1
+
+
+print(f"""Has seleccionado el planeta {namePlanet}, este planeta ocupa la posición número {posPlanet+1} en el sistema solar
+Los planetas que lo anteceden son {planetas[0:posPlanet]}
+Los planetas posteriores son {planetas[posPlanet+1:]}""")
