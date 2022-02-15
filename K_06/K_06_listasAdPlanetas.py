c7b665c92c0aba5e0f6cd7a38692447b12b26048
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 23:24:42 -0600

    agrega exepciones a primer y ultimo planeta

--- K_06/K_06_listasAdPlanetas.py
+++ K_06/K_06_listasAdPlanetas.py
@@ -22,7 +22,20 @@ try:
 except: 
     posPlanet = -1
 
+# print(len(planetas))
+# print(posPlanet)
 
-print(f"""Has seleccionado el planeta {namePlanet}, este planeta ocupa la posición número {posPlanet+1} en el sistema solar
-Los planetas que lo anteceden son {planetas[0:posPlanet]}
-Los planetas posteriores son {planetas[posPlanet+1:]}""")
+if posPlanet == 0:
+    print(f"""Has seleccionado el planeta {namePlanet}, este planeta ocupa la posición número {posPlanet+1} en el sistema solar
+    Ningun planeta lo antecede
+    Los planetas posteriores son {planetas[posPlanet+1:]}""")
+
+elif posPlanet == len(planetas)-1:
+    print(f"""Has seleccionado el planeta {namePlanet}, este planeta ocupa la posición número {posPlanet+1} en el sistema solar
+    Los planetas que lo anteceden son {planetas[0:posPlanet]}
+    Este es el ultimo planeta""")
+
+else:
+    print(f"""Has seleccionado el planeta {namePlanet}, este planeta ocupa la posición número {posPlanet+1} en el sistema solar
+    Los planetas que lo anteceden son {planetas[0:posPlanet]}
+    Los planetas posteriores son {planetas[posPlanet+1:]}""")
