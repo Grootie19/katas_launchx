b3b1e39231ee0733f3dc3578b7eff41d47c1d9f6
isbaruc <launchx05286@innovaccion.mx>
2022-02-14 19:31:12 -0600

    convierte inputs en int

--- K_05/K_05_inputDistPlanetas.py
+++ K_05/K_05_inputDistPlanetas.py
@@ -7,4 +7,11 @@ inNamePlanet_02 = input("Nombre del segundo Planeta")
 inDistPlanet_01 = input("Distancia al sol (primer planeta)")
 inDistPlanet_02 = input("Distancia al sol (segundo planeta)")
 
+# Input devulve valores de tipo cadena
+
+#Convertir cadenas a números
+inNamePlanet_01 = int(inNamePlanet_01)
+inNamePlanet_02 = int(inNamePlanet_02)
+inDistPlanet_01 = int(inDistPlanet_01)
+inDistPlanet_02 = int(inDistPlanet_02)
 
