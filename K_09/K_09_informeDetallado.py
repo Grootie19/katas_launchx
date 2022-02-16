5a8530510f437892e3c3dda6706f385d273867d8
isbaruc <launchx05286@innovaccion.mx>
2022-02-16 17:50:29 -0600

    Agrega funcion imprime informe

--- K_09/K_09_informeDetallado.py
+++ K_09/K_09_informeDetallado.py
@@ -6,4 +6,14 @@
 # Tanque externo
 # Tanque interno
 
+def informeMision (hLaunch, tVuelo, nDestino, cTanqueExt, cTanqueInt):
+    
+    return f""""La hora de lanzamiento aproximada es: {hLaunch}
+    Tiempo de vuelo esperado: {tVuelo}
+    Destino:  {nDestino}
+    Capacidad del tanque externo: {cTanqueExt}
+    Capacidad del tanque interno: {cTanqueInt}
+    """
+
+print(informeMision('14:00', 50, 'Luna', 800, 500))
 
