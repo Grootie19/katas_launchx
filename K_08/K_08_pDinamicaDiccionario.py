c4559da88ed09c9327cb31c8fd902059b75cdfc0
isbaruc <launchx05286@innovaccion.mx>
2022-02-15 20:18:45 -0600

    Crea diccionario

---
+++ K_08/K_08_pDinamicaDiccionario.py
@@ -0,0 +1,19 @@
+# Calcular el número total de lunas en el SS
+# Calcila el promedio de lunas que tiene un planeta
+
+# Planetas y lunas
+
+nMoonPlanet = {
+    'mercurio' : 0,
+    'venus' : 0,
+    'tierra' : 1,
+    'marte' : 2,
+    'jupiter' : 79,
+    'saturno' : 82,
+    'urano' : 27,
+    'neptuno' : 14,
+    'pluton' : 5,
+    'haumea' : 2,
+    'makemake' : 1,
+    'eris' : 1,
+}
