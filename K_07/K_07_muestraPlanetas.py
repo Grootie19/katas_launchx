7adf749caab68a2571961c32e5392fdc55e05f0a
isbaruc <launchx05286@innovaccion.mx>
2022-02-15 18:41:45 -0600

    Agrega ciclo for para mostrar lista de planetas

--- K_07/K_07_muestraPlanetas.py
+++ K_07/K_07_muestraPlanetas.py
@@ -1,2 +1,19 @@
 # Mostrar la lista de planetas que el usuario haya ingresado
 
+# Codigo anterior
+# Declaracion de variables
+
+new_planet = ''
+planets = []
+
+# Para terminar la lista el usuario ingresa la palabra 'done'
+while new_planet.lower() != 'done':
+    planets.append(new_planet)
+    new_planet = input("Nombre del planeta a agregar: ")
+
+planets.pop(0)
+
+item = 0
+for planeta in planets:
+    print(f"{item+1}.- {planets[item]}")
+    item += 1
