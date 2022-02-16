ce1ef4f5777b2a0baf706a740c38296917dd35f0
isbaruc <launchx05286@innovaccion.mx>
2022-02-16 17:38:53 -0600

    Agrega instrucciones del ejercicio

---
+++ K_09/K_09_informeDetallado.py
@@ -0,0 +1,9 @@
+# Informe detallado del estado de combustible haciendo uso de argumentos de palabra clave
+# Aspectos que deberá incluir el informe
+# Hora de prelanzamiento
+# Tiempo de vuelo
+# Destino
+# Tanque externo
+# Tanque interno
+
+
